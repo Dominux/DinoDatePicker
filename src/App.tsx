d186255f62a55e69a0a621ce5a14d1f22b862531
Dominux <kir.gimranov@gmail.com>
2023-04-16 14:01:57 +0300

    fixed bug with wrong ru localisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker'
-import dayjs from 'dayjs'
-import 'dayjs/locale/ru'
-import 'dayjs/locale/en'
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import dayjs from "dayjs";
+import "dayjs/locale/ru";
+import "dayjs/locale/en";
 
-const adapterLocale = navigator.language === 'ru-Ru' ? 'ru' : 'en'
+const adapterLocale = navigator.language.startsWith("ru") ? "ru" : "en";
 
 export default function App() {
-  const now = dayjs()
+	const now = dayjs();
 
-  return (
-    <LocalizationProvider
-      dateAdapter={AdapterDayjs}
-      adapterLocale={adapterLocale}
-    >
-      <StaticDatePicker autoFocus defaultValue={now} />
-    </LocalizationProvider>
-  )
+	return (
+		<LocalizationProvider
+			dateAdapter={AdapterDayjs}
+			adapterLocale={adapterLocale}
+		>
+			<StaticDatePicker autoFocus defaultValue={now} />
+		</LocalizationProvider>
+	);
 }
